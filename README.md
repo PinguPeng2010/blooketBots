b8a7741cc9637ed4192703fe6db75564d3e3024d
pingupengi2010 <93376708+PinguPeng2010@users.noreply.github.com>
2026-06-11 21:21:40 +0100

    extra readme and new stuff

---
+++ README.md
@@ -0,0 +1,33 @@
+# blooketBotNet
+
+`blooketBotNet.py` is a small Python automation script that opens multiple browser tabs and uses keyboard and screen input to interact with a Blooket session.
+
+## Overview
+
+- The script is built around timed browser automation and pixel checks. 
+- It prompts for the number of bots, a name prefix, a starting number, and a game code.
+- The bot then attempts to connect
+- The script then will cull bots if they have not connected of if they have panicked.
+
+## Requirements
+
+The script depends on Python and the following packages:
+
+- `pyautogui`
+- `pynput`
+- `mss`
+
+- An implementation to auto install the scripts will happen.
+
+## Reliability
+
+This program is only about 60% effective. Its success depends on website loading times, so results can vary between runs and even between individual tabs.
+
+## Repository Notes
+
+- The main script is [blooketBotNet.py](blooketBotNet.py).
+
+## Caution
+
+- Not my fault if u get banned lol
+- Maybe use without your accoung on
--- blooketBotNet.py
+++ blooketBotNet.py
@@ -7,21 +7,22 @@ import random
 
 
 
-code = (input("Enter the code: "))
+
 
 botNumber = int(input("Enter the number of bots: "))
 
 botName = input("Enter the name of the bots: ")
 
 botNameStartNumber = input("Enter the starting number for the bots: ")
+code = (input("Enter the code: "))
 
 # Opens bots
 for x in range(0,botNumber):
     webbrowser.open_new_tab("https://play.blooket.com/play")
 
-    print(f'Opened {x} google tabs')
+    print(f'Spawned {x+1} blooket bots')
 
-time.sleep(0.5)
+time.sleep(1)
 
 keyboard = Controller() # For pynput
 
@@ -47,19 +48,15 @@ for x in range(int(botNameStartNumber),botNumber + int(botNameStartNumber)):
 
     time.sleep(0.2) # Same here, it is too fast, it caches the loading pages.
 
-    pyautogui.typewrite('bot'+ str(x) , interval=0)
+    pyautogui.typewrite(botName+ str(x) , interval=0)
     pyautogui.press('enter')
-
+    print(f'{botName +str(x)} initialized')
 
     with keyboard.pressed(Key.ctrl):
         keyboard.press(Key.tab)
         keyboard.release(Key.tab)
 
-time.sleep()
 # Now it just clicks stuff
-# Once game starts, random picks a number from 1-4, and pyautogui types it. It then moves to the next tab.
-# After every bot has clicked, we go click, choose a number from one to 3, and click a position, and click again.
-# Using mss, the bot will check if a pixel is red. If so
 
 # First, inactive tabs (bots that haven't connected) are closed by checking the rgb value of a pixel.
 
@@ -75,19 +72,41 @@ def detectPixel(x, y):
 
 # On blooket, the background changes to a solid purple when connected. 111, 165 is the coord used.
 
-for bot in range(0, botNumber):
+culled = 0
 
-    if detectPixel(111, 165) != (154, 73, 170):    # If the pixel is not purple, the bot is inactive, so we close it.
+for bot in range(0, botNumber):
+    pixelColour = detectPixel(111, 165)
+    print(f'Bot {botName +str(bot+int(botNameStartNumber))} pixel colour is {pixelColour}')
+
+
+    if  pixelColour != (154, 73, 170):    # If the pixel is not purple, the bot is inactive, so we close it.
 
         with keyboard.pressed(Key.ctrl):
             keyboard.press('w')
             keyboard.release('w')
+
+        culled += 1
+        print(f'Bot {botName +str(bot+int(botNameStartNumber))} was culled')
 
     else:
 
         with keyboard.pressed(Key.ctrl):    # If it is the right colour move to next tab.
             keyboard.press(Key.tab)
             keyboard.release(Key.tab)
+            print(f'Bot {botName +str(bot+int(botNameStartNumber))} is safe')
+    time.sleep(0.1)
 
 
+print(f'{culled} bots culled')
+
+
+# The script now asks if the user wants to autoc answer questions.
+
+wantAutoAnswer = input("Do you want to auto answer questions? (y/n): ")
+
+# TODO: Auto answer the questions using random
+# TODO: Use mss to detect if the background is green to click.
+# TODO: Use pyautogui to click boxes randomly for the bots.
+# TODO: Use mss to detect if a swap or steal is selected, and use pyautogui to auto click the top right corner one.
+# TODO: detect if the game is finshed using mss and auto ending the script.
 
